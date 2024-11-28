0fef15cd0be75133d310119bd3479647b80b201b
erniedadea29 <edadea@gbox.ncf.edu.ph>
2024-11-28 17:34:11 +0800

    Update Education.jsx

--- src/Education.jsx
+++ src/Education.jsx
@@ -39,14 +39,14 @@ function Education() {
             borderRadius: '10px',
             boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
             textAlign: 'center',
-            backgroundColor: 'transparent', // Ensure no white background
+            backgroundColor: 'transparent', // No background for the section
           }}
         >
           <h3 style={{ fontSize: '1.5rem', marginBottom: '10px', color: '#5754ff' }}>Elementary</h3>
           <p style={{ fontSize: '1rem', color: '#013220' }}>Tabuco Central School Graduation: March of 2015</p>
 
-          <Carousel>
-            <Carousel.Item>
+          <Carousel style={{ backgroundColor: 'transparent' }}>
+            <Carousel.Item style={{ backgroundColor: 'transparent' }}>
               <img
                 className="d-block w-100"
                 src="https://scontent.fwnp1-1.fna.fbcdn.net/v/t1.15752-9/467460283_1654335702156044_7122622365438454931_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeHemIf_ZipqfQKSTtGxcsajlBVO_JrIjjyUFU78msiOPNx-cM5zrKhHVQzcjfXrfzaxtCvzZLxfgUUC82LVLWFx&_nc_ohc=WTM1ToYc2kUQ7kNvgHbLkO6&_nc_zt=23&_nc_ht=scontent.fwnp1-1.fna&oh=03_Q7cD1QGpcdivy2j3zvE7jW1H_oXiLWZmrhYlVN93A6KeQ-8yBA&oe=67679CF6"
@@ -55,14 +55,14 @@ function Education() {
                   height: '200px',
                   objectFit: 'cover',
                   borderRadius: '8px',
-                  backgroundColor: 'transparent', // Remove any white background from image
+                  backgroundColor: 'transparent', // Ensure image has no white background
                 }}
               />
               <Carousel.Caption>
                 <p>Elementary Image 1</p>
               </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item style={{ backgroundColor: 'transparent' }}>
               <img
                 className="d-block w-100"
                 src="https://scontent.fwnp1-1.fna.fbcdn.net/v/t1.15752-9/467465084_1087725006143211_7816802892559952915_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeGq26odKWfQjTsok33fXWblAGlwYDBkA6oAaXBgMGQDqrIMlXLKcTOybuHE23TlcQUskpyJocj055g2rjYrxirc&_nc_ohc=VshtogOaux4Q7kNvgEK9oTa&_nc_zt=23&_nc_ht=scontent.fwnp1-1.fna&oh=03_Q7cD1QFf-jL978xIRx3OgjNPVD7hJ-6tBqgvEHJq2S0i8kMQGA&oe=6767B3E2"
@@ -71,14 +71,14 @@ function Education() {
                   height: '200px',
                   objectFit: 'cover',
                   borderRadius: '8px',
-                  backgroundColor: 'transparent', // Remove any white background from image
+                  backgroundColor: 'transparent', // Ensure image has no white background
                 }}
               />
               <Carousel.Caption>
                 <p>Elementary Image 2</p>
               </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item style={{ backgroundColor: 'transparent' }}>
               <img
                 className="d-block w-100"
                 src="https://scontent.fwnp1-1.fna.fbcdn.net/v/t1.15752-9/467465673_928667892020356_9115596917301806628_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeG6KQvhTNsVs3_P5ZHR6LtiLMJSAtdlMK4swlIC12UwrowLSLhYiEWp9iBi9f12lhRvQhETYa4swOBs2iLpp9Fo&_nc_ohc=ZgBIyzWoIIkQ7kNvgFzrZCX&_nc_zt=23&_nc_ht=scontent.fwnp1-1.fna&oh=03_Q7cD1QEgZLWf37AFcBHwf3MMsblHlJorC6JR1xBmBUhFubIHfQ&oe=676796CF"
@@ -87,7 +87,7 @@ function Education() {
                   height: '200px',
                   objectFit: 'cover',
                   borderRadius: '8px',
-                  backgroundColor: 'transparent', // Remove any white background from image
+                  backgroundColor: 'transparent', // Ensure image has no white background
                 }}
               />
               <Carousel.Caption>
@@ -104,14 +104,14 @@ function Education() {
             borderRadius: '10px',
             boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
             textAlign: 'center',
-            backgroundColor: 'transparent', // Ensure no white background
+            backgroundColor: 'transparent', // No background for the section
           }}
         >
           <h3 style={{ fontSize: '1.5rem', marginBottom: '10px', color: '#5754ff' }}>Highschool</h3>
           <p style={{ fontSize: '1rem', color: '#013220' }}>Camarines Sur National Highschool Graduation: May of 2021</p>
 
-          <Carousel>
-            <Carousel.Item>
+          <Carousel style={{ backgroundColor: 'transparent' }}>
+            <Carousel.Item style={{ backgroundColor: 'transparent' }}>
               <img
                 className="d-block w-100"
                 src="https://scontent.fwnp1-1.fna.fbcdn.net/v/t1.15752-9/385559669_1041920940190140_8208293206348743845_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeHF3JGHvbrK7NnwxFzznDUfR0tGGtda4GJHS0Ya11rgYsmZgZ3pO45lVQENHrf0HMyBAvs17lcNlfE_y8WYAWq_&_nc_ohc=szJLJWtBYJQQ7kNvgF7Lw27&_nc_zt=23&_nc_ht=scontent.fwnp1-1.fna&oh=03_Q7cD1QHOD8CpQ86vCpTm-2pfvzD_7kB3JthFHqPtCnCN20RJdQ&oe=67678F7D"
@@ -120,14 +120,14 @@ function Education() {
                   height: '200px',
                   objectFit: 'cover',
                   borderRadius: '8px',
-                  backgroundColor: 'transparent', // Remove any white background from image
+                  backgroundColor: 'transparent', // Ensure image has no white background
                 }}
               />
               <Carousel.Caption>
                 <p>Highschool Image 1</p>
               </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item>
+            <Carousel.Item style={{ backgroundColor: 'transparent' }}>
               <img
                 className="d-block w-100"
-                src="https://scontent.fwnp1-1.fna.fbcdn.net/v/t1.15752-9/467460289_592701946568002_1882644795066672202_n.png?_nc_cat=108&ccb=1-7&_nc_sid=9f807c&_nc_eui2=AeEMW4eR8EPhBtTtABxlVhW8QxuDube7JBpDG4O5t7skGhks5PNPgCQfMcZ1iUfIzvsZ-n9yu23dFkLekM3icgf8&_nc_ohc=hO0ql58SxycQ7kNvgFBOW4z&_nc_zt=23&_nc_ht=scontent.fwnp1-1.fna&oh=03_Q7cD1QG074m0Fa8C1X62MS57Yt54g0jTem
+                src="https://scontent.fwnp1-1.fna.fbcdn.net/v/t1.15752-9/467460289_592701946568002_1882644795066672202_n.png?_nc_cat=108&
