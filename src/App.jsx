dab6046df183a1002270379c906b1784be4ef5ca
erniedadea29 <edadea@gbox.ncf.edu.ph>
2024-11-28 21:52:25 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <div>
       <Router>
-        <Navbar expand="lg" style={{ backgroundColor: '#5754ff' }}>
+       <Navbar expand="lg" style={{ backgroundColor: '#5754ff' }}>
           <Container>
             <Navbar.Brand as={Link} to="/" style={{ color: 'white', fontWeight: 'bold' }}>
               MY PERSONAL INFO
