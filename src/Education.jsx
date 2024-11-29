6187a9421a8f063c2a1337751d19dc2dcc63dde6
erniedadea29 <edadea@gbox.ncf.edu.ph>
2024-11-29 21:07:46 +0800

    Update Education.jsx

--- src/Education.jsx
+++ src/Education.jsx
@@ -35,7 +35,7 @@ function Education() {
         <div
           style={{
             backgroundColor: 'white',
-            padding: '80px',
+            padding: '20px',
             borderRadius: '10px',
             boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
             textAlign: 'center',
