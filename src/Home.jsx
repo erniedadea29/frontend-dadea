7ae820f116ee5bac1dcf43fa3d92ac51018487c9
erniedadea29 <edadea@gbox.ncf.edu.ph>
2024-11-29 14:40:22 +0800

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
       id="about"
       style={{
         padding: '50px',
-        backgroundColor: '#013220',
+        backgroundColor: '#0d3634',
         width: '100%',
         height: '100vh', 
         boxSizing: 'border-box',
